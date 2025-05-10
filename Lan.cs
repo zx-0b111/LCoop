db8068636428a3ac8da45d3e48a4f6c3147c91f3
Zx <148573313+zx-0b111@users.noreply.github.com>
2025-05-10 04:48:15 -0300

    Add files via upload

---
+++ Lan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Unity.Netcode.Transports.UTP;
+using Unity.Netcode;
+
+
+namespace TutorialMod
+{
+    [HarmonyPatch(typeof(MenuManager))]
+    [HarmonyPatch(("StartAClient"))]
+    
+    public class Lan
+    {
+        const ushort porta = 7777;
+        
+        
+        [HarmonyPrefix]
+
+        static void Prefix()
+        {
+            using (StreamReader sr = new StreamReader("BepInEx\\plugins\\LConf.txt"))
+            {
+                string ip = sr.ReadLine();
+                var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+                transport.SetConnectionData(ip, porta);
+
+                Console.WriteLine(ip);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace TutorialMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Tutorial : BaseUnityPlugin
+    {
+        private const string modGUID = "zx.LCoop";
+        private const string modName = "LCoop";
+        private const string modVersion = "1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        void Awake()
+        {
+            var registrador = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            registrador.LogMessage(modName + " carregou com sucesso");
+
+            harmony.PatchAll(typeof(Tutorial));
+            harmony.PatchAll(typeof(Lan));
+        }
+    }
+}
+
